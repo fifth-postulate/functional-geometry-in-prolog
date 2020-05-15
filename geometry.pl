92b99b5e82f69feea237944ecc17cbf2d15bc310
Daan van Berkel <daan.van.berkel@alliander.com>
2020-05-15 20:40:28 +0200

    extract open svg end close svg

--- geometry.pl
+++ geometry.pl
@@ -246,4 +246,11 @@ shape(d,
 */
 
 svg -->
-    "<svg xmlns=""http://www.w3.org/2000/svg""></svg>".
+    open_svg,
+    close_svg.
+
+open_svg -->
+    "<svg xmlns=""http://www.w3.org/2000/svg"">".
+
+close_svg -->
+    "</svg>".
